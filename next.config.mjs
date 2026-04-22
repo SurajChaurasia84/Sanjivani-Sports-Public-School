8cf5a30aaa0ba0d925a61e3aabd615870b00998d
Suraj Chaurasia <suraj84chaurasia@gmail.com>
2026-04-22 20:51:42 +0530

    Deploy to github pages

--- next.config.mjs
+++ next.config.mjs
@@ -5,8 +5,8 @@ const nextConfig = {
     unoptimized: true,
   },
   // If you are deploying to https://<username>.github.io/<repository-name>/
-  // basePath: '/Sanjivani-Sports-Public-School',
-  // assetPrefix: '/Sanjivani-Sports-Public-School',
+  basePath: '/Sanjivani-Sports-Public-School',
+  assetPrefix: '/Sanjivani-Sports-Public-School',
 };
 
 export default nextConfig;
